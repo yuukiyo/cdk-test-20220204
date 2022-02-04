731c43af3437a7cc62cfb69eed963d2e0f64ce8d
Yuki Yoshida <yuyoshd@amazon.co.jp>
2022-02-04 09:09:39 +0900

    Initial commit

---
+++ test/cdk-test-20220204.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkTest20220204 from '../lib/cdk-test-20220204-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-test-20220204-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkTest20220204.CdkTest20220204Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
